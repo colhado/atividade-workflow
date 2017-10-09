6b7af379fc383a890945cb53ac68662e9deaa628
colhado <gustavocolhado@hotmail.com>
2017-10-08 22:41:58 -0300

    fim da atividade

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,13 @@ var sass = require('gulp-sass');
 /* Variáveis Sass */
 var scssFiles = './source/scss/*.scss';
 var cssDest = './dist/css/';
-var sassDevOptions = {
-  outputStyle: 'expanded'
-}
 var sassProdOptions = {
   outputStyle: 'compressed'
 }
 
 /* Funções */
-gulp.task('sass-develop', function() {
-  return gulp.src(scssFiles)
-    .pipe(sass(sassDevOptions).on('error', sass.logError))
-    .pipe(gulp.dest(cssDest));
-});
 
-gulp.task('sass-production', function() {
+gulp.task('sass', function() {
   return gulp.src(scssFiles)
     .pipe(sass(sassProdOptions).on('error', sass.logError))
     .pipe(gulp.dest(cssDest));
@@ -32,8 +24,8 @@ gulp.task('move-html', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(scssFiles, ['sass-develop', 'sass-production']);
+  gulp.watch(scssFiles, ['sass']);
   gulp.watch('./source/index.html', ['move-html']);
 });
 
-gulp.task('default', ['sass-develop', 'sass-production', 'watch']);
+gulp.task('default', ['sass', 'watch']);
